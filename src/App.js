864e54bc2f793eaa95146c92639f24fbe93dd6bf
Fredrik93 <fredrikullman93@gmail.com>
2020-02-22 20:35:03 +0100

    add Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import Table from './Table'
-
+import { render } from '@testing-library/react'
 
 class App extends Component {
+    state = {
+        characters: [],
+    }
+    removeCharacter = index => {
+        const { characters } = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
     render() {
+        const { characters } = this.state
+
         return (
             <div className="container">
-                <Table />
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
             </div>
         )
     }
 }
+
+
 
 export default App;
