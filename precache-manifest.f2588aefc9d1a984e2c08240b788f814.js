fcb4757cf8a3b4efbc0b299cadd8a372cd8af0b8
Fredrik93 <fredrikullman93@gmail.com>
2020-02-23 06:50:01 +0100

    Updates

---
+++ precache-manifest.f2588aefc9d1a984e2c08240b788f814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aeb94e9f085dd2b37709cbcf6e28e09",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "72ff10daf5d643bdf1da",
+    "url": "/react-tutorial/static/css/main.87e0e2f8.chunk.css"
+  },
+  {
+    "revision": "27ad37b837848d430e6a",
+    "url": "/react-tutorial/static/js/2.b016d13e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial/static/js/2.b016d13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ff10daf5d643bdf1da",
+    "url": "/react-tutorial/static/js/main.8ab6114e.chunk.js"
+  },
+  {
+    "revision": "dd2aac3cba69db775e8b",
+    "url": "/react-tutorial/static/js/runtime-main.b5a976d7.js"
+  }
+]);
